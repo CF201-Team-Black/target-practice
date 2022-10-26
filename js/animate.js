43affac0a384481df0bde81cc9ce5d5ed1c1cade
ianfshirley <ianfshirley88@gmail.com>
2022-10-26 13:51:01 -0700

    fixed cursor position in gameplay

--- js/animate.js
+++ js/animate.js
@@ -8,13 +8,13 @@ function getRandomNum(min, max) {
 
 function setRandomXPosition() {
   let root = document.documentElement;
-  let newFirstX = getRandomNum(400, 720);
+  let newFirstX = getRandomNum(600, 1160);
   root.style.setProperty('--first-x-position', newFirstX + 'px');
-  let newSecondX = getRandomNum(0, 320);
+  let newSecondX = getRandomNum(0, 560);
   root.style.setProperty('--second-x-position', newSecondX + 'px'); 
-  let newThirdX = getRandomNum(400, 720);
+  let newThirdX = getRandomNum(600, 1160);
   root.style.setProperty('--third-x-location', newThirdX + 'px');
-  let newFourthX = getRandomNum(0, 320);
+  let newFourthX = getRandomNum(0, 560);
   root.style.setProperty('--fourth-x-postion', newFourthX + 'px');
 }
 
