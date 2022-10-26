4582edca875bb8b758d35ac71ae2f14264a422f6
ianfshirley <ianfshirley88@gmail.com>
2022-10-26 16:10:37 -0700

    fixed font and updated buttons in game.html, also fixed bird animation to keep it inside play area

--- js/animate.js
+++ js/animate.js
@@ -8,13 +8,13 @@ function getRandomNum(min, max) {
 
 function setRandomXPosition() {
   let root = document.documentElement;
-  let newFirstX = getRandomNum(600, 1160);
+  let newFirstX = getRandomNum(500, 960);
   root.style.setProperty('--first-x-position', newFirstX + 'px');
-  let newSecondX = getRandomNum(0, 560);
+  let newSecondX = getRandomNum(0, 460);
   root.style.setProperty('--second-x-position', newSecondX + 'px'); 
-  let newThirdX = getRandomNum(600, 1160);
+  let newThirdX = getRandomNum(500, 960);
   root.style.setProperty('--third-x-location', newThirdX + 'px');
-  let newFourthX = getRandomNum(0, 560);
+  let newFourthX = getRandomNum(0, 460);
   root.style.setProperty('--fourth-x-postion', newFourthX + 'px');
 }
 
